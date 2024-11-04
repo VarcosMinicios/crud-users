f80b33f264f7a4ddf3d62ff9be7ec53c2f3eff57
VarcosMinicios <marcos11vini2@outlook.com>
2024-11-04 07:25:13 -0300

    feat: config migrations

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,13 +1,15 @@
 import { Options, MySqlDriver } from '@mikro-orm/mysql';
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
+import { Migrator } from '@mikro-orm/migrations';
 
 const config: Options = {
   driver: MySqlDriver,
   dbName: 'mikro_orm_test',
+  extensions: [ Migrator ],
   user: 'root',
   password: 'root',
-  entities: ['dist/**/*.entity.js'],
-  entitiesTs: ['src/**/*.entity.ts'],
+  entities: [ 'dist/**/*.entity.js' ],
+  entitiesTs: [ 'src/**/*.entity.ts' ],
   metadataProvider: TsMorphMetadataProvider,
   debug: true,
 };
